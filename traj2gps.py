120ee6666e2b503f6036c4aedfded27b06966225
Piero Toffanin <pt@masseranolabs.com>
2020-07-01 21:37:19 +0000

    Data read, interpolation

--- traj2gps.py
+++ traj2gps.py
@@ -7,15 +7,17 @@ import datetime
 import json
 import shutil
 import csv
+import numpy as np
 from gwpy.time import tconvert
+from scipy.interpolate import interp1d
 from PIL import Image
 import piexif
 
 parser = argparse.ArgumentParser(description='Trajectory to GPS')
 parser.add_argument('--crs', '-c',
-                    metavar='EPSG:xxxx',
+                    metavar='<string>',
                     required=True,
-                    help='EPSG code of trajectory values')
+                    help='EPSG code of trajectory values (EPSG:xxxx)')
 parser.add_argument('--input', '-i',
                     metavar='<path>',
                     required=True,
@@ -24,6 +26,13 @@ parser.add_argument('--trajectory', '-t',
                     metavar='<path>',
                     required=True,
                     help='Path of trajectory file')
+parser.add_argument('--interpolation', '-interp',
+                    metavar='<string>',
+                    required=False,
+                    choices=['linear', 'quadratic', 'cubic'],
+                    default='linear',
+                    help='Interpolation method for trajectory points. %(default)s')
+
 # parser.add_argument('amount',
 #                     metavar='<pixel|percentage%>',
 #                     type=str,
@@ -39,7 +48,7 @@ if exiftool_path is None:
     die("exiftool not found. Is it installed?")
 
 def die(msg):
-    print(msg)
+    print("ERR: %s" % msg)
     exit(1)
 
 files = []
@@ -58,9 +67,9 @@ def ddb(*args):
 def exiftool(*args):
     return subprocess.check_output([exiftool_path] + list(args)).decode('utf8')
 
+print("Searching: %s files" % len(files))
 print("Gathering image information")
 images = []
-
 
 #h_tmp, tmp_path = tempfile.mkstemp("ddb")
 #os.close(h_tmp)
@@ -70,8 +79,16 @@ images = []
 #    images = json.loads(f.read())
 
 # TODO: ddb cannot handle FLIR tags, yet...
+img_min_t = np.Inf
+img_max_t = -np.Inf
 out = exiftool(*files, "-s", "-s", "-s", "-DateTimeOriginal")
 lines = [l.strip() for l in out.split("\n")]
+
+# Handle case with single file
+if len(files) == 1:
+    lines.insert(0, "======== %s" % files[0])
+    lines.append("")
+
 lines = lines[:-2]
 it = iter(lines)
 for line in it:
@@ -84,10 +101,15 @@ for line in it:
             dto = dto[:-6] + dto[-6:].replace(":", "")
 
         date = datetime.datetime.strptime(dto, '%Y:%m:%d %H:%M:%S.%f%z')
+        utctime = date.timestamp()
+
         images.append({
             'path': file,
-            'utctime': date.timestamp()
+            'utctime': utctime
         })
+
+        img_min_t = min(img_min_t, utctime)
+        img_max_t = max(img_max_t, utctime)
 
 if len(images) == 0:
     die("No images")
@@ -98,15 +120,50 @@ if len(images) != len(files):
     print("WARNING: some files could not be parsed")
 
 print("Reading trajectory file")
-trajectories = []
+trajectories = [[], [], []]
+times = []
+# gps_times = []
+
 with open(args.trajectory) as csvfile:
     reader = csv.DictReader(csvfile)
+    i = 0
     for row in reader:
-        time = float(row['Time[s]']) + 1e9 # Adjusted GPS time
+        gps_time = float(row['Time[s]']) + 1e9 # Adjusted GPS time
         easting = float(row['Easting[m]'])
         northing = float(row['Northing[m]'])
         height = float(row['Height[m]'])
-        utctime = tconvert(time).timestamp()
 
-        trajectories.append([utctime, easting, northing, height])
+        utctime = tconvert(gps_time).timestamp()
+        
+        # gps_times.append(gps_time)
+        times.append(utctime)
 
+        trajectories[0].append(easting)
+        trajectories[1].append(northing)
+        trajectories[2].append(height)
+        
+        if i % 1000 == 0:
+            print(".", end ="", flush=True)
+        i += 1
+    print("")
+
+traj_min_t = np.min(times)
+traj_max_t = np.max(times)
+
+# print(np.min(gps_times))
+# print(np.max(gps_times))
+
+# Quick check on interpolation values...
+print("Images time range: %s" % [img_min_t, img_max_t])
+print("Trajectories time range: %s" % [traj_min_t, traj_max_t])
+
+if img_min_t < traj_min_t:
+    die("Image min range < Trajectories min range. The images timestamps fall outside of the available times in the trajectory file.")
+
+if img_max_t > traj_max_t:
+    die("Image max range > Trajectories max range. The images timestamps fall outside of the available times in the trajectory file.")
+
+print("Interpolating GPS positions")
+interpolator = interp1d(times, trajectories, kind=args.interpolation)
+for img in images:
+    print("%s --> %s" % (img['path'], interpolator(img['utctime'])))
